1693d56e06fcb196a6e70c5443527417f8e2792a
jeanfrancoisalbertini <74418226+jeanfrancoisalbertini@users.noreply.github.com>
2021-01-21 16:36:57 +0100

    Update Tiles.java

--- src/com/company/Tiles.java
+++ src/com/company/Tiles.java
@@ -1,7 +1,32 @@
 package com.company;
-
+import java.util.Random;
 public class Tiles {
     int orientation;
-    boolean recto=true;
-    String nomsuspect; 
+    boolean recto = true;
+    String color;
+    public Tiles(String color, int orientation){
+        this.color = color;
+        this.orientation = orientation;
+    }
+    public void setOrientation(int orientation) {
+        this.orientation = orientation;
+    }
+    public int getOrientation() {
+        return orientation;
+    }
+
+
+    public boolean isRecto() {
+        return recto;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getColor() {
+        return color;
+    }
+
+
+
 }
